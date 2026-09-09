133979f072b4baf0419000c3a113f19aa8a9e1a7
Kis <you@example.com>
2026-09-09 23:17:51 +0800

    Student Controller

---
+++ src/controllers/studentController.js
@@ -0,0 +1,64 @@
+const students = [
+  {
+    id: 1,
+    name: "Rio Pana",
+    age: 21,
+    course: "Computer Science",
+  },
+  {
+    id: 2,
+    name: "Nesline Diaz",
+    age: 21,
+    course: "Computer Science",
+  },
+  {
+    id: 3,
+    name: "Kate Ronda",
+    age: 21,
+    course: "Computer Science",
+  },
+  {
+    id: 4,
+    name: "Karen Clair Probadora",
+    age: 21,
+    course: "Computer Science",
+  },
+];
+
+export const getStudents = (req, res) => {
+  res.send(students);
+};
+
+export const getStudentById = (req, res) => {
+  const student = students.find((s) => s.id === parseInt(req.params.id));
+  if (!student) return res.status(404).send("Student not found");
+  res.send(student);
+};
+
+export const createStudent = (req, res) => {
+  const newStudent = {
+    id: students.length ? students[students.length - 1].id + 1 : 1,
+    name: req.body.name,
+    age: req.body.age,
+    course: req.body.course,
+  };
+
+  students.push(newStudent);
+  res.send(newStudent);
+};
+
+export const updateStudent = (req, res) => {
+  const student = students.find((s) => s.id === parseInt(req.params.id));
+  if (!student) return res.status(404).send({ message: "Student not found" });
+
+  Object.assign(student, req.body);
+  res.send(student);
+};
+
+export const deleteStudent = (req, res) => {
+  const studentIndex = students.findIndex((s) => s.id === parseInt(req.params.id));
+  if (studentIndex === -1) return res.status(404).send({ message: "Student not found" });
+
+  const deletedStudent = students.splice(studentIndex, 1);
+  res.send(deletedStudent[0]);
+};
