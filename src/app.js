9f944777f4dff4f09f14b02ff847370266ee8edd
Ohriyo <rio.pana09@gmail.com>
2026-09-10 21:57:29 +0800

    Fixed to contents for app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,10 @@
 import express from "express";
-import {
-  getStudents,
-  getStudentById,
-  createStudent,
-  updateStudent,
-  deleteStudent,
-} from "./controllers/studentController.js";
+import studentRoutes from "./src/routes/studentRoutes.js";
 
 const app = express();
 app.use(express.json());
 
-app.get("/student", getStudents);
-app.get("/student/:id", getStudentById);
-app.post("/student", createStudent);
-app.patch("/student/:id", updateStudent);
-app.delete("/student/:id", deleteStudent);
+app.use(studentRoutes);
 
 export default app;
 
